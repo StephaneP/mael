e84ce017faa824fee13c7f96b42eed71c0343eb7
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-05-08 17:52:36 +0100

    feat: 🎸 Add initial tools to timeline
    TODO: Run something to autocrop the tech logos to content

---
+++ components/atoms/TechLogo/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import styles from "./styles.module.css";
+
+const TechLogo: FC<{ name: string }> = ({ name }) => {
+  return (
+    <div className={styles.container}>
+      <div
+        style={{ backgroundImage: `url(/images/tech/logos/${name}.png)` }}
+        className={styles.img}
+      />
+    </div>
+  );
+};
+
+export default TechLogo;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import FancyCard from "../components/atoms/FancyCard";
 import Socials from "../components/atoms/Socials";
+import TechLogo from "../components/atoms/TechLogo";
 import CardContainer from "../components/atoms/CardContainer";
 import Card from "../components/atoms/Card";
 import Timeline from "../components/atoms/Timeline";
@@ -42,10 +43,38 @@ export default function IndexPage() {
                 key: "thrds-1",
                 title: (
                   <>
-                    <em>Threads Styling</em> London | Software Engineer
+                    <p>
+                      <em>Threads Styling</em> London | Software Engineer
+                    </p>
                   </>
                 ),
-                description: <>August 2018 - Present</>,
+                description: (
+                  <>
+                    <p className={styles.datetime}>August 2018 - Present</p>
+                    <div className={styles.techContainer}>
+                      <TechLogo name="nodejs" />
+                      <TechLogo name="js" />
+                      <TechLogo name="react" />
+                      <TechLogo name="react-native" />
+                      <TechLogo name="jira" />
+                      <TechLogo name="splunk" />
+                      <TechLogo name="postgresql" />
+                      <TechLogo name="sentry" />
+                      <TechLogo name="git" />
+                      <TechLogo name="github" />
+                      <TechLogo name="aws" />
+                      <TechLogo name="terraform" />
+                      <TechLogo name="circleci" />
+                      <TechLogo name="graphql" />
+                      <TechLogo name="typescript" />
+                      <TechLogo name="jest" />
+                      <TechLogo name="webpack" />
+                      <TechLogo name="yarn" />
+                      <TechLogo name="npm" />
+                      <TechLogo name="docker" />
+                    </div>
+                  </>
+                ),
                 icon: "/images/company/logos/threads-styling.png",
               },
               {
@@ -55,7 +84,23 @@ export default function IndexPage() {
                     <em>MailOnline</em> London | Software Engineer
                   </>
                 ),
-                description: <>March 2018 - July 2018</>,
+                description: (
+                  <>
+                    <p className={styles.datetime}>March 2018 - July 2018</p>
+                    <div className={styles.techContainer}>
+                      <TechLogo name="react" />
+                      <TechLogo name="nodejs" />
+                      <TechLogo name="js" />
+                      <TechLogo name="git" />
+                      <TechLogo name="github" />
+                      <TechLogo name="rabbitmq" />
+                      <TechLogo name="teamcity" />
+                      <TechLogo name="npm" />
+                      <TechLogo name="redis" />
+                      <TechLogo name="splunk" />
+                    </div>
+                  </>
+                ),
                 icon: "/images/company/logos/mailonline.png",
               },
               {
@@ -65,7 +110,22 @@ export default function IndexPage() {
                     <em>MailOnline</em> London | Junior Software Engineer
                   </>
                 ),
-                description: <>July 2017 - March 2018</>,
+                description: (
+                  <>
+                    <p className={styles.datetime}>July 2017 - March 2018</p>
+                    <div className={styles.techContainer}>
+                      <TechLogo name="nodejs" />
+                      <TechLogo name="js" />
+                      <TechLogo name="git" />
+                      <TechLogo name="github" />
+                      <TechLogo name="rabbitmq" />
+                      <TechLogo name="teamcity" />
+                      <TechLogo name="npm" />
+                      <TechLogo name="redis" />
+                      <TechLogo name="splunk" />
+                    </div>
+                  </>
+                ),
                 icon: "/images/company/logos/mailonline.png",
               },
               {
@@ -75,7 +135,28 @@ export default function IndexPage() {
                     <em>Clock Limited</em> Kings Langley | Junior Software Engineer
                   </>
                 ),
-                description: <>July 2016 - July 2017</>,
+                description: (
+                  <>
+                    <p className={styles.datetime}>July 2016 - July 2017</p>
+                    <div className={styles.techContainer}>
+                      <TechLogo name="nodejs" />
+                      <TechLogo name="js" />
+                      <TechLogo name="react" />
+                      <TechLogo name="mongodb" />
+                      <TechLogo name="git" />
+                      <TechLogo name="github" />
+                      <TechLogo name="backbone" />
+                      <TechLogo name="jade" />
+                      <TechLogo name="freshdesk" />
+                      <TechLogo name="php" />
+                      <TechLogo name="mysql" />
+                      <TechLogo name="drone" />
+                      <TechLogo name="mocha" />
+                      <TechLogo name="npm" />
+                      <TechLogo name="gulp" />
+                    </div>
+                  </>
+                ),
                 icon: "/images/company/logos/clock.png",
               },
               {
@@ -83,11 +164,30 @@ export default function IndexPage() {
                 title: <>Loughborough University | Computer Science MSci Year 5</>,
                 description: (
                   <>
-                    <p>2012-2013</p>
+                    <p className={styles.datetime}>2012-2013</p>
                     <p>
                       <em>Thesis:</em> Complex Adaptive Systems and Grouping Behaviour over
                       Evolution
                     </p>
+                    <div className={styles.techContainer}>
+                      <TechLogo name="python" />
+                      <TechLogo name="docker" />
+                      <TechLogo name="git" />
+                      <TechLogo name="github" />
+                    </div>
+                    <p>
+                      <em>evoto</em>
+                    </p>
+                    <div className={styles.techContainer}>
+                      <TechLogo name="react" />
+                      <TechLogo name="blockchain" />
+                      <TechLogo name="multichain" />
+                      <TechLogo name="csharp" />
+                      <TechLogo name="wpf" />
+                      <TechLogo name="webpack" />
+                      <TechLogo name="git" />
+                      <TechLogo name="github" />
+                    </div>
                   </>
                 ),
                 icon: "/images/company/logos/lboro-uni.png",
@@ -99,7 +199,27 @@ export default function IndexPage() {
                     <em>Clock Limited</em> Kings Langley | Placement Software Engineer
                   </>
                 ),
-                description: <>July 2015 - July 2016</>,
+                description: (
+                  <>
+                    <p className={styles.datetime}>July 2015 - July 2016</p>
+                    <div className={styles.techContainer}>
+                      <TechLogo name="nodejs" />
+                      <TechLogo name="js" />
+                      <TechLogo name="mongodb" />
+                      <TechLogo name="git" />
+                      <TechLogo name="github" />
+                      <TechLogo name="backbone" />
+                      <TechLogo name="jade" />
+                      <TechLogo name="freshdesk" />
+                      <TechLogo name="php" />
+                      <TechLogo name="mysql" />
+                      <TechLogo name="drone" />
+                      <TechLogo name="mocha" />
+                      <TechLogo name="npm" />
+                      <TechLogo name="gulp" />
+                    </div>
+                  </>
+                ),
                 icon: "/images/company/logos/clock.png",
               },
               {
@@ -107,11 +227,27 @@ export default function IndexPage() {
                 title: <>Loughborough University | Computer Science MSci Year 3</>,
                 description: (
                   <>
-                    <p>2012-2013</p>
+                    <p className={styles.datetime}>2012-2013</p>
                     <p>
                       <em>Dissertation:</em> Stock Market Prediction using Historical Data and News
                       Information
                     </p>
+                    <div className={styles.techContainer}>
+                      <TechLogo name="nodejs" />
+                      <TechLogo name="js" />
+                      <TechLogo name="mongodb" />
+                      <TechLogo name="git" />
+                      <TechLogo name="github" />
+                      <TechLogo name="jade" />
+                    </div>
+                    <p>
+                      <em>Other:</em>
+                    </p>
+                    <div className={styles.techContainer}>
+                      <TechLogo name="java" />
+                      <TechLogo name="android" />
+                      <TechLogo name="aspnet" />
+                    </div>
                   </>
                 ),
                 icon: "/images/company/logos/lboro-uni.png",
@@ -121,8 +257,14 @@ export default function IndexPage() {
                 title: <>Loughborough University | Computer Science MSci Year 2</>,
                 description: (
                   <>
-                    <p>2012-2013</p>
+                    <p className={styles.datetime}>2012-2013</p>
                     <p>My second year</p>
+                    <div className={styles.techContainer}>
+                      <TechLogo name="mysql" />
+                      <TechLogo name="java" />
+                      <TechLogo name="ruby" />
+                      <TechLogo name="haskell" />
+                    </div>
                   </>
                 ),
                 icon: "/images/company/logos/lboro-uni.png",
@@ -132,7 +274,7 @@ export default function IndexPage() {
                 title: <>Loughborough University | Computer Science MSci Year 1</>,
                 description: (
                   <>
-                    <p>2012-2013</p>
+                    <p className={styles.datetime}>2012-2013</p>
                     <p>My first year</p>
                   </>
                 ),
