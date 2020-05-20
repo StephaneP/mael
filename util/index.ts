96c572f1842eb2c04ecf81980e912f28f116345b
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-05-20 10:52:53 +0100

    fix: 🐛 Better post sorting, fix tag user selection

--- util/index.ts
+++ util/index.ts
@@ -1,5 +1,7 @@
+import compareDesc from "date-fns/compareDesc";
+
 export function sortDates(field: string) {
   return (p1: any, p2: any) => {
-    return new Date(p1[field]).getUTCMilliseconds() - new Date(p2[field]).getUTCMilliseconds();
+    return compareDesc(new Date(p1[field]), new Date(p2[field]));
   };
 }
