9739db10544e949682dfa4b0d4686de8c16d93ee
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-05-20 12:30:27 +0100

    feat: 🎸 Initial work on react native click blog post

---
+++ components/atoms/EventExample/index.tsx
@@ -0,0 +1,67 @@
+import { createRef, useEffect, useState } from "react";
+import styles from "./styles.module.css";
+
+const listOfEvents = [
+  "click",
+  "mousedown",
+  "mouseup",
+  "focus",
+  "blur",
+  "keydown",
+  "change",
+  "dblclick",
+  "keydown",
+  "keyup",
+  "keypress",
+  "textInput",
+  "touchstart",
+  "touchmove",
+  "touchend",
+  "touchcancel",
+  "resize",
+  "scroll",
+  "zoom",
+  "select",
+  "change",
+  "submit",
+  "reset",
+];
+
+export default function EventExample() {
+  const ref = createRef<HTMLButtonElement>();
+  const [list, setList] = useState<string[]>([]);
+  useEffect(() => {
+    const addEvent = (eventType: string) => {
+      return () => setList((l) => [eventType].concat(l.slice(0, 4)));
+    };
+    if (ref.current) {
+      listOfEvents.forEach((eventType) => {
+        ref.current.addEventListener(eventType, addEvent(eventType));
+      });
+    }
+    return () => {
+      if (ref.current) {
+        listOfEvents.forEach((eventType) => {
+          ref.current.removeEventListener(eventType, addEvent(eventType));
+        });
+      }
+    };
+  }, []);
+  return (
+    <div className={styles.container}>
+      <div className={styles.btnContainer}>
+        <button ref={ref} className={styles.button}>
+          ✨ Click me! ✨
+        </button>
+      </div>
+      <div className={styles.events}>
+        {list.map((e, i) => [
+          <div key={`${e}${i}`} className={styles.event}>
+            {e}
+          </div>,
+          i === list.length - 1 ? null : <div className={styles.eventSpacer}>then</div>,
+        ])}
+      </div>
+    </div>
+  );
+}
