a35ddb27d809b883e1f1f99ca95909d89804b0d4
Matthew Elphick <matt.a.elphy@gmail.com>
2020-10-11 18:12:43 +0100

    feat: Flesh out initial pages of gibberish, add some content

--- components/layouts/bookpage/gibberish/contents.ts
+++ components/layouts/bookpage/gibberish/contents.ts
@@ -7,16 +7,84 @@ export default {
   "2": {
     name: "Background of Programming",
     url: "/books/gibberish/2/background",
-    sections: {},
-  },
-  "3": {
-    name: "What is a website",
-    url: "/books/gibberish/3/what-is-a-website",
     sections: {
       "2": {
-        name: "Frontend",
-        url: "/books/gibberish/3/frontend",
+        name: "What is a website",
+        url: "/books/gibberish/2/what-is-a-website",
+      },
+      "3": {
+        name: "How the Web Works",
+        url: "/books/gibberish/2/how-the-web-works",
       },
     },
+  },
+  "3": {
+    name: "Frontend Basics",
+    url: "/books/gibberish/3/frontend-basics",
+    sections: {
+      "2": {
+        name: "HTML - The Content",
+        url: "/books/gibberish/3/html",
+      },
+      "3": {
+        name: "CSS - Making HTML look good",
+        url: "/books/gibberish/3/css",
+      },
+    },
+  },
+  "4": {
+    name: "JavaScript",
+    url: "/books/gibberish/4/javascript-intro",
+    sections: {
+      "2": {
+        name: "Data Types",
+        url: "/books/gibberish/4/datatypes",
+      },
+      "3": {
+        name: "Variables",
+        url: "/books/gibberish/4/variables",
+      },
+      "4": {
+        name: "Control Flow",
+        url: "/books/gibberish/4/control-flow",
+      },
+      "5": {
+        name: "Functions",
+        url: "/books/gibberish/4/functions",
+      },
+      "6": {
+        name: "Arrays",
+        url: "/books/gibberish/4/arrays",
+      },
+      "7": {
+        name: "Objects",
+        url: "/books/gibberish/4/objects",
+      },
+    },
+  },
+  "5": {
+    name: "Asynchronous",
+    url: "/books/gibberish/5/async",
+    sections: {},
+  },
+  "6": {
+    name: "Setting up your tools",
+    url: "/books/gibberish/6/dev-env",
+    sections: {},
+  },
+  "7": {
+    name: "Terminal",
+    url: "/books/gibberish/7/terminal",
+    sections: {},
+  },
+  "8": {
+    name: "Version Control",
+    url: "/books/gibberish/8/version-control",
+    sections: {},
+  },
+  "9": {
+    name: "Backend Basics",
+    url: "/books/gibberish/9/backend",
+    sections: {},
   },
 };
