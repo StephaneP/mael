fc405350a7335e861e629fee521437129d0378ac
Matthew Elphick <matthew.elphick@threadsstyling.com>
2021-05-09 19:46:45 +0100

    fix: 🐛 Update meta card colours

--- src/util/getPostCard.ts
+++ src/util/getPostCard.ts
@@ -3,7 +3,7 @@ import { capitalize } from "./strings";
 export default function getPostCard(t: string) {
   const title = capitalize(t);
   const api = "https://i.microlink.io/";
-  const cardUrl = `https://cards.microlink.io/?preset=rauchg&logo=https%3A%2F%2Fmael.tech%2Fimages%2Fme.jpeg&domain=mael.tech&color=%23fd015d&bg=%23202020&title=${title}`;
+  const cardUrl = `https://cards.microlink.io/?preset=rauchg&logo=https%3A%2F%2Fmael.tech%2Fimages%2Fpaw_programming.jpg&domain=mael.tech&color=%23db2777&bg=%231f2937&title=${title}`;
   const image = `${api}${encodeURIComponent(cardUrl)}`;
   return image;
 }
