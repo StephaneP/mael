31a66dba3d73f03fa2a4843f6984f879372b9883
Matthew Elphick <matthew.elphick@threadsstyling.com>
2021-05-10 15:10:25 +0100

    fix: 🐛 Scroll to top on route change
    Should investigate this more to fix the route cause, but not sure how
    possible that is with the custom scrollbars. And I like the scrollbars.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createRef } from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { DefaultSeo } from "next-seo";
@@ -7,8 +7,18 @@ import useFathom from "../components/hooks/useFathom";
 import { ThemeProvider } from "../components/contexts/theme";
 import "../styles/main.css";
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps, router }: AppProps) {
   useFathom();
+  const scrollableEl = createRef<HTMLDivElement>();
+  React.useEffect(() => {
+    function handleChange() {
+      if (scrollableEl.current) scrollableEl.current.scrollTo(0, 0);
+    }
+    router.events.on("routeChangeComplete", handleChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleChange);
+    };
+  });
   return (
     <>
       <Head>
@@ -19,7 +29,10 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#db2777" />
       </Head>
       <DefaultSeo {...SEO} />
-      <div className="max-h-full overflow-y-auto sm:scrollbar-thin sm:scrollbar-thumb-pink-600 sm:scrollbar-track-gray-700">
+      <div
+        ref={scrollableEl}
+        className="max-h-full overflow-y-auto sm:scrollbar-thin sm:scrollbar-thumb-pink-600 sm:scrollbar-track-gray-700"
+      >
         <ThemeProvider>
           <Component {...pageProps} />
         </ThemeProvider>
