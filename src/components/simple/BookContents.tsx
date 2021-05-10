d6b1742cbb6c351c37ebddb8c0c0841e32f457e2
Matthew Elphick <matthew.elphick@threadsstyling.com>
2021-05-10 10:00:16 +0100

    fix: 🐛 Tidier book contents

--- src/components/simple/BookContents.tsx
+++ src/components/simple/BookContents.tsx
@@ -8,12 +8,14 @@ export default function BookContent({ contents }: any) {
         return (
           <div key={chapter} className="flex flex-row gap-2">
             <Link href={content.link}>
-              <a>{chapter}</a>
+              <a>{chapter}.</a>
             </Link>
             <div className="flex flex-col gap-1">
               {Object.entries(content.items).map(([name, item]: any) => (
                 <Link key={`${chapter}-${name}`} href={item.link}>
-                  <a>{name}</a>
+                  <a>
+                    {item.section}. {item.title}
+                  </a>
                 </Link>
               ))}
             </div>
